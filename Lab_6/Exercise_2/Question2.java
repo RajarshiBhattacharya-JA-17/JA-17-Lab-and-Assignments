a247972fc687b6352dee7b73341b5bc9fb25e612
RajarshiBhattacharya-JA-17 <84121054+RajarshiBhattacharya-JA-17@users.noreply.github.com>
2021-06-02 16:02:12 +0530

    Update Question2.java

--- Lab_6/Exercise_2/Question2.java
+++ Lab_6/Exercise_2/Question2.java
@@ -2,10 +2,15 @@ package com.capgemini.question2;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.Map.Entry;
 
 public class Question2 {
 	
-	public static Map<Character, Integer> countChars(char[] arr){
+	static Scanner sc = new Scanner(System.in);
+	static Map<Character, Integer> charCount = new HashMap<Character, Integer>();
+	
+	private static Map<Character, Integer> countChars(char[] arr){
 		
 		Map<Character, Integer> charCount = new HashMap<Character, Integer>();
 		for(char c : arr) {
@@ -20,5 +25,32 @@ public class Question2 {
 		
 		return charCount;
 	}
+	
+	public static void readChars() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter number of characters to enter:");
+		int n = sc.nextInt();
+		char[] arr = new char[n];
+		
+		for(int i=0; i<n; i++) {
+			System.out.println("Enter Character:");
+			arr[i] = sc.next().trim().charAt(0);
+		}
+		
+		charCount = countChars(arr);
+		System.out.println();
+		displayCount();
+	}
+
+	private static void displayCount() {
+		// TODO Auto-generated method stub
+		 System.out.println("Charcater Count:");
+		 for(Entry<Character, Integer> hashmapdata : charCount.entrySet()) {
+			 System.out.print(hashmapdata);
+			 System.out.print(", ");
+			 System.out.println();
+		 }
+	}
+
 
 }
